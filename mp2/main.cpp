b4b332305ed3a86174593846523f80ccc9796ed5
Unknown <beaucha3@illinois.edu>
2017-03-21 14:19:00 -0500

    Fixed gettimeofday errors

--- mp2/main.cpp
+++ mp2/main.cpp
@@ -5,6 +5,7 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
@@ -30,15 +31,15 @@ int main(int argc, char** argv)
 		fprintf(stderr, "Usage: %s mynodeid initialcostsfile logfile\n\n", argv[0]);
 		exit(1);
 	}
-	
-	//initialization: get this process's node ID, record what time it is, 
+
+	//initialization: get this process's node ID, record what time it is,
 	//and set up our sockaddr_in's for sending to the other nodes.
 	globalMyID = atoi(argv[1]);
 	int i;
 	for(i=0;i<256;i++)
 	{
 		gettimeofday(&globalLastHeartbeat[i], 0);
-		
+
 		char tempaddr[100];
 		sprintf(tempaddr, "10.1.1.%d", i);
 		memset(&globalNodeAddrs[i], 0, sizeof(globalNodeAddrs[i]));
@@ -46,9 +47,9 @@ int main(int argc, char** argv)
 		globalNodeAddrs[i].sin_port = htons(7777);
 		inet_pton(AF_INET, tempaddr, &globalNodeAddrs[i].sin_addr);
 	}
-	
+
 	//TODO: read and parse initial costs file. default to cost 1 if no entry for a node. file may be empty.
-	
+
 	//socket() and bind() our socket. We will do all sendto()ing and recvfrom()ing on this one.
 	if((globalSocketUDP=socket(AF_INET, SOCK_DGRAM, 0)) < 0)
 	{
@@ -57,7 +58,7 @@ int main(int argc, char** argv)
 	}
 	char myAddr[100];
 	struct sockaddr_in bindAddr;
-	sprintf(myAddr, "10.1.1.%d", globalMyID);	
+	sprintf(myAddr, "10.1.1.%d", globalMyID);
 	memset(&bindAddr, 0, sizeof(bindAddr));
 	bindAddr.sin_family = AF_INET;
 	bindAddr.sin_port = htons(7777);
@@ -68,11 +69,11 @@ int main(int argc, char** argv)
 		close(globalSocketUDP);
 		exit(1);
 	}
-	
+
 	//start threads... feel free to add your own, and to remove the provided ones.
 	pthread_t announcerThread;
 	pthread_create(&announcerThread, 0, announceToNeighbors, (void*)0);
-	
+
 	//good luck, have fun!
 	listenForNeighbors();
 }
--- mp2/monitor_neighbors.cpp
+++ mp2/monitor_neighbors.cpp
@@ -5,6 +5,7 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
