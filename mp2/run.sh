fb93156390967db2a41caca5192441a6cefd1d91
Atrifex <rishirt.us@gmail.com>
2017-03-27 20:40:48 -0500

    Fixed bug in run.sh

--- mp2/run.sh
+++ mp2/run.sh
@@ -9,7 +9,7 @@ DEFAULT_TOPO_FILE="topoexample.txt"
 TOPO_DIR=${1:-$DEFAULT_TOPO_DIR}
 TOPO_FILE_PREFIX=${2:-$DEFAULT_TOPO_FILE_PREFIX}
 TOPO_FILE=${3:-$DEFAULT_TOPO_FILE}
-INIT_COSTS=$(find $(TOPO_DIR) -maxdepth -1 -name "${TOPO_FILE_PREFIX}*" -print)
+INIT_COSTS=$(find $(TOPO_DIR) -maxdepth 1 -name "${TOPO_FILE_PREFIX}*" -print)
 
 pkill ls_router
 make clean
