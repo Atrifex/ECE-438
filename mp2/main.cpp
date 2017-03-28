52ffd6c2f90035b9702a4f88430cb4dc6f0d0f78
Atrifex <rishirt.us@gmail.com>
2017-03-27 20:28:54 -0500

    Added todo list

--- mp2/main.cpp
+++ mp2/main.cpp
@@ -8,6 +8,17 @@ int main(int argc, char** argv)
         exit(1);
     }
 
+    /*
+     * TODO:
+     *      1) handle send (Rishi)
+     *      2) Logging output for grading and debugging (Nathan)
+     *      3) Test with neighbors (together)
+     *      4) look at heartbeats (Whoever gets here first)
+     *      5) send lsp (together)
+     *          - avoid flooding
+     *          - might require using variation of lsp
+     */
+
     // Parse input and initialize router
     LS_Router router(atoi(argv[1]), argv[2]);
 
