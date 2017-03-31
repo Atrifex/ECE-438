3c43836e212f5133b83510e6194f7463020dec81
Atrifex <rishirt.us@gmail.com>
2017-03-31 08:35:57 -0500

    Updated test and grade scripts

--- mp2/scripts/grade.sh
+++ mp2/scripts/grade.sh
@@ -9,7 +9,7 @@ TOPO_DIR=${DEFAULT_TOPO_DIR}
 TOPO_FILE_PREFIX=${DEFAULT_TOPO_FILE_PREFIX}
 GRAPH_DISP=$(find ${TOPO_DIR} -maxdepth 1 -name "${TOPO_FILE_PREFIX}*" -print)
 
-FILE_END=${1:--1}
+GOLD_FILE=${1:-"./example_topology/goldNetwork.txt"}
 
 if [ ${FILE_END} -eq -1 ]; then
     echo "Specify node to compare to" 
@@ -19,10 +19,18 @@ fi
 pkill ls_router
 sudo iptables --flush
 
-echo "Starting grading"
+echo "Starting grading:"
 
-
+TOPO_PASSED=1
 for FILE_NAME in ${GRAPH_DISP}; do
-    echo ${FILE_NAME}
-    diff  ${TOPO_DIR}${TOPO_FILE_PREFIX}${FILE_END} ${FILE_NAME}
+    if ! diff -q "${GOLD_FILE}" "${FILE_NAME}"; then
+        echo "${FILE_NAME} differs from gold output"
+        TOPO_PASSED=0
+    fi
 done
+
+if [ TOPO_PASSED -eq 1 ]; then
+    echo "All output files converged. PASS!"
+else
+    echo "Files did not converge. FAIL!"
+fi
--- mp2/test.sh
+++ mp2/test.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 usage(){
         echo "usage: ./test.sh [--help | -h]
-                 [--test-default | -t] [--test-custom] [--test-new]
-                 [--run-default | -t] [--run-custom] [--run-new]
-                 [--grade-default | -t] [--grade-custom] [--grade-new]
+                 [--test-default | -t] [--test-generated] [--test-new]
+                 [--test-custom <folder> <initcost_prefix> <topofilename>]
+                 [--run-default | -t] [--run-generated] [--run-new]
+                 [--run-custom <folder> <initcost_prefix> <topofilename>]
+                 [--grade-default | -t] [--grade-generated] [--grade-new]
+                 [--grade-custom <folder> <initcost_prefix> <topofilename> <gold_out>]
                  [--new-graph | -n] "
 }
 
@@ -18,7 +21,7 @@ do
             python ./scripts/topotest.py ./example_topology test2initcosts topoexample.txt
             exit
             ;;
-        --test-custom)
+        --test-generated)
             python ./scripts/topotest.py ./topology nodecosts networkTopology.txt
             exit
             ;;
@@ -27,11 +30,16 @@ do
             python ./scripts/topotest.py ./topology nodecosts networkTopology.txt
             exit
             ;;
+        --test-custom)
+            python ./scripts/topotest.py $2 $3 $4
+            exit
+            shift; shift; shift;
+            ;;
         --run-default | -r)
             sh ./scripts/run.sh
             exit
             ;;
-        --run-custom)
+        --run-generated)
             sh ./scripts/run.sh ./topology nodecosts networkTopology.txt
             exit
             ;;
@@ -40,24 +48,36 @@ do
             sh ./scripts/run.sh ./topology nodecosts networkTopology.txt
             exit
             ;;
+        --run-custom)
+            sh ./scripts/run.sh $2 $3 $4
+            exit
+            shift; shift; shift;
+            ;;
         --grade-default | -g)
             sh ./scripts/run.sh
             sleep 7s
             sh ./scripts/grade.sh
             exit
             ;;
-        --grade-custom) 
+        --grade-generated) 
             sh ./scripts/run.sh ./topology nodecosts networkTopology.txt
             sleep 7s
-            sh ./scripts/grade.sh
+            sh ./scripts/grade.sh ./topology/goldNetwork.txt
             exit
             ;;
         --grade-new)
             python ./scripts/createTopology.py
             sh ./scripts/run.sh ./topology nodecosts networkTopology.txt
             sleep 7s
-            sh ./scripts/grade.sh # TODO: Change inputs
+            sh ./scripts/grade.sh ./topology/goldNetwork.txt
             exit
+            ;;
+        --grade-custom)
+            sh ./scripts/run.sh $2 $3 $4
+            sleep 7s
+            sh ./scripts/grade.sh $5
+            exit
+            shift; shift; shift; shift;
             ;;
         --new-graph | -n)
             python ./scripts/createTopology.py
