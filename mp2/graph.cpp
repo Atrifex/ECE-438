3813f8f4caa77dfb5f73fb3414d426c0ebf55021
Atrifex <rishirt.us@gmail.com>
2017-04-01 15:28:32 -0500

    Got rid of warning

--- mp2/graph.cpp
+++ mp2/graph.cpp
@@ -155,7 +155,7 @@ vector<int> Graph::dijkstra()
     }
 
     for(size_t i = 0; i < NUM_NODES; i++){
-        if(predecessor[i] == INVALID && i != myNodeID) {
+        if(predecessor[i] == INVALID && i != (size_t)myNodeID) {
             for(size_t j = 0; j < NUM_NODES; j++)
                 valid[i][j] = false;
         }
